ede6530ea85ae3b2dda6e2b8231ad1de5c874da5
Asgerfyr <asger.g.stidsen@gmail.com>
2025-04-20 15:26:14 +0200

    login request separate file

--- milWeb/app.js
+++ milWeb/app.js
@@ -1,6 +1,5 @@
 var createError = require('http-errors');
 var express = require('express');
-const session = require('express-session');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -20,35 +19,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 // Dummy User Data (Replace with a real database) ######################################################################
-const users = { "8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918": "8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918",
-  "57b6c3bcc81dd2a6595aba6f9697bda014b2c57301c5745ff924c60dc2a97cb0": "57b6c3bcc81dd2a6595aba6f9697bda014b2c57301c5745ff924c60dc2a97cb0"
- };
-
 const permissionUsers = {"8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918":2,
   "57b6c3bcc81dd2a6595aba6f9697bda014b2c57301c5745ff924c60dc2a97cb0":1,
 };
 
+const loginRoutes = require('./routes/login.js');
+
 // Authentication Route (Login)
-app.post('/login', (req, res) => {
-  const { username, password } = req.body;
-  console.log(req.body);
-  
-  if (users[username] && users[username] === password) {
-      req.session.user = { username };
-      req.session.userId = username;
-      console.log(req.session.user);
-      return res.json({ message: "Login successful" });
-  }
-  console.log("Invalid credentials");
-  res.status(401).json({ error: "Invalid credentials" });
-});
-//######################################################################################################################
-
-// Middleware to Protect Routes
-
-app.get('/login', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'login.html'));
-});
+app.use('/login', loginRoutes);
 
 // Protected Route (Dashboard)
 app.get('/main', (req, res) => {
